8d21e5157c157fceaf5dd66cbdbb6c87016a6ee7
HichamOujari <hichamoujari99@gmail.com>
2022-03-15 19:23:48 +0100

    sdlsd

--- src/preload.js
+++ src/preload.js
@@ -7,20 +7,23 @@ window.addEventListener("DOMContentLoaded", () => {
       var absolutePath = document.getElementById("setDefaultDirectory").value;
       var embdedCode = document.getElementById("textareaId").value;
       var filename = document.getElementById("filenameId").value;
-      if (embdedCode.length == 0) alert("Enter your embed code");
+      if (absolutePath.length == 0) alert("Enter directory path");
       else {
-        if (filename.length == 0) alert("Enter File name");
+        if (embdedCode.length == 0) alert("Enter your embed code");
         else {
-          const videoId = embdedCode
-            .split('<iframe src="https://cdn.viqeo.tv/embed/?vid=')[1]
-            .split('"')[0];
-          ipcRenderer.invoke(
-            "save-files",
-            absolutePath,
-            filename,
-            embdedCode,
-            videoId
-          );
+          if (filename.length == 0) alert("Enter File name");
+          else {
+            const videoId = embdedCode
+              .split('<iframe src="https://cdn.viqeo.tv/embed/?vid=')[1]
+              .split('"')[0];
+            ipcRenderer.invoke(
+              "save-files",
+              absolutePath,
+              filename,
+              embdedCode,
+              videoId
+            );
+          }
         }
       }
     },
--- src/renderer.js
+++
@@ -1,2 +0,0 @@
-const path = window.location.pathname.split("/")
-document.getElementById("setDefaultDirectory").value = path[0]+path.slice(1, -1).join("/")
